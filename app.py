d5dacabf291f1e4cdfb1d7b2980698aa4633a233
fffreestanford <fffreestanford@gmail.com>
2025-01-25 13:36:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import plotly.express as px
 def load_data():
     pi_project_count = pd.read_csv('https://raw.githubusercontent.com/fffreestanford/stanford-sponsored-research/refs/heads/main/processed/pi_project_count.csv')
     projects = pd.read_csv('https://raw.githubusercontent.com/fffreestanford/stanford-sponsored-research/refs/heads/main/data/Project_List_Report_2005_2024.csv',
-                           encoding='latin1')
+                           encoding='utf-8')
     congo_companies = pd.read_csv('https://raw.githubusercontent.com/fffreestanford/stanford-sponsored-research/refs/heads/main/data/congo_sponsors.csv')
     fossil_companies = pd.read_csv('https://raw.githubusercontent.com/fffreestanford/stanford-sponsored-research/refs/heads/main/data/fossil_sponsors.csv')
     return pi_project_count, projects, congo_companies, fossil_companies
